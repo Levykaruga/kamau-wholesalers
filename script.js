4b4955f71433ea8fe08fe88f1a31fda6692f0d40
Levykaruga <levykaruga@gmail.com>
2026-06-11 21:36:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Kamau Wholesalers Website Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+console.log("Kamau Wholesalers Dashboard Loaded");
+
+document.querySelectorAll("button").forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+alert(
+"Quotation request feature coming soon."
+);
+
+});
+
+});
+
+});
